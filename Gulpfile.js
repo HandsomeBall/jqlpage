44e296cf5937e4b5593e32912098309de4d72ac6
HandsomeBall <3152149160@qq.com>
2015-11-12 01:30:17 +0800

    展示基本数据

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,8 @@ var jshint = require('gulp-jshint');  // 语法检查   （gulp-jshint）
 var concat = require('gulp-concat');  // 合并文件   （gulp-concat）
 var uglify = require('gulp-uglify');  // 压缩代码   （gulp-uglify）
 var rename = require('gulp-rename');  // 文件重命名 （gulp-rename）
+var react = require('gulp-react');  // react （gulp-react）
+// var babel = require('gulp-babel');  // EcmaScript6 (gulp-babel)
 
 // 语法检查
 gulp.task('jshint', function () {
@@ -13,12 +15,13 @@ gulp.task('jshint', function () {
 
 // 合并文件之后压缩代码
 gulp.task('minify', function (){
-     return gulp.src('src/**/*.js')
-        .pipe(concat('jqlpage.js'))
-        .pipe(gulp.dest('dist/js'))
-        .pipe(uglify())
-        .pipe(rename('jqlpage.min.js'))
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src("src/**/*.js*")
+    .pipe(react())
+    .pipe(concat('jqlpage.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename('jqlpage.min.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 // html 文件复制
@@ -30,12 +33,12 @@ gulp.task('copy-html', function() {
 // res 文件复制
 gulp.task('copy-res', function() {
   return gulp.src('res/**')
-    .pipe(gulp.dest('dist/res'));
+    .pipe(gulp.dest('dist'));
 });
 
 // 监视文件的变化
 gulp.task('watch', function () {
-    gulp.watch('src/*.js', ['jshint', 'minify']);
+    gulp.watch('src/**', ['copy-res', 'copy-html', 'jshint', 'minify']);
 });
 
 // 注册缺省任务
--- src/js/page.js
+++ src/js/page.js
@@ -1,1 +1,59 @@
-console.log("ok");
+var game;  // phaser 游戏实例
+var graphics;  // phaser rectangle drawer
+var marginLeft = 80;
+var marginTop = 80;
+var flagWidth = 100;
+var flagHeight = 50;
+var flagMargin = 10;
+
+var flags = [
+  ["司令", "司令", "司令", "司令", "司令"],
+  ["司令", "", "司令", "", "司令"],
+  ["司令", "司令", "", "司令", "司令"],
+  ["司令", "", "司令", "", "司令"],
+  ["司令", "司令", "司令", "司令", "司令"],
+  ["司令", "司令", "司令", "司令", "司令"]
+];
+
+function makeFlag(text, pos) {
+  if (text) {
+    // [i, j] = pos;
+    var i = pos[0];
+    var j = pos[1];
+    var bmd = game.add.bitmapData(flagWidth, flagHeight);
+    bmd.rect(0, 0, flagWidth, flagHeight, '#55ff55');
+    bmd.text("司令", 15, 25, '20px 宋体', 'black', false);
+    drawnObject = game.add.sprite(
+      marginLeft + (flagWidth + 2 * flagMargin) * j,
+      marginTop + (flagHeight + 2 * flagMargin) * i,
+      bmd
+    );
+    drawnObject.anchor.setTo(0.5, 0.5);
+    return drawnObject;
+  }
+}
+
+function preload() {
+}
+
+function create() {
+  for(var i in flags) {
+    for(var j in flags[i]) {
+      makeFlag(flags[i][j], [i, j]);
+    }
+  }
+  var input = game.input; //当前游戏的input对象
+  input.addMoveCallback(function(){
+    console.log(input.mousePointer);
+  }, this);
+  // var signal = input.onDown; //鼠标按下时的 Signal对象
+  // signal.add(function(){}); //给Signal 绑定事件处理函数
+}
+
+function update() {
+
+}
+
+$(function() {
+  game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: preload, create: create, update: update });
+});
